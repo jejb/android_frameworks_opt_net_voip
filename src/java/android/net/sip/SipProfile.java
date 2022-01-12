3de5065d89014d42a338f7ef8497994a17ce14dc
James Bottomley <James.Bottomley@HansenPartnership.com>
2022-01-12 15:56:47 -0500

    SipProfile: add OutgoingPhoneAccount flag
    This is used by the Telephony service to reset the outgoing account to
    be a SIP account if it is enabled.
    
    Change-Id: I0ae103296d3c0043dcc6fb6949d782708e8ddc51
    Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>

--- src/java/android/net/sip/SipProfile.java
+++ src/java/android/net/sip/SipProfile.java
@@ -62,6 +62,8 @@ public class SipProfile implements Parcelable, Serializable, Cloneable {
     private int mRegistrationTimeout = 3600;
     private boolean mSendKeepAlive = false;
     private boolean mAutoRegistration = true;
+    // can only be set on *one* SIP account
+    private boolean mOutgoingPhoneAccount = false;
     private transient int mCallingUid = 0;
 
     public static final Parcelable.Creator<SipProfile> CREATOR =
@@ -289,6 +291,11 @@ public class SipProfile implements Parcelable, Serializable, Cloneable {
             return this;
         }
 
+        public Builder setOutgoingPhoneAccount(boolean flag) {
+            mProfile.mOutgoingPhoneAccount = flag;
+            return this;
+        }
+
         /**
          * Builds and returns the SIP profile object.
          *
@@ -339,6 +346,7 @@ public class SipProfile implements Parcelable, Serializable, Cloneable {
         mPort = in.readInt();
         mAuthUserName = in.readString();
         mRegistrationTimeout = in.readInt();
+        mOutgoingPhoneAccount = (in.readInt() == 0) ? false : true;
     }
 
     @Override
@@ -355,6 +363,7 @@ public class SipProfile implements Parcelable, Serializable, Cloneable {
         out.writeInt(mPort);
         out.writeString(mAuthUserName);
         out.writeInt(mRegistrationTimeout);
+        out.writeInt(mOutgoingPhoneAccount ? 1 : 0);
     }
 
     @Override
@@ -505,6 +514,10 @@ public class SipProfile implements Parcelable, Serializable, Cloneable {
      */
     public boolean getAutoRegistration() {
         return mAutoRegistration;
+    }
+
+    public boolean getOutgoingPhoneAccount() {
+        return mOutgoingPhoneAccount;
     }
 
     /**
